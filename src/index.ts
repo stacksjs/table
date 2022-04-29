9af8ebbbf627aa51ac06e0caaedcd3050a57f976
Chris <chrisbreuer93@gmail.com>
2022-04-28 17:38:07 -0700

    chore: wip

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { defineCustomElement } from 'vue'
-import Table from './components/TableV2.ce.vue'
-import TableConfigure from './components/TableConfigure.ce.vue'
-
-// convert into custom element constructor
-const TableCustomElement = defineCustomElement(Table)
-const TableConfigureCustomElement = defineCustomElement(TableConfigure)
-
-// export individual elements
-// export { TableCustomElement, TableConfigureCustomElement };
-
-// export function register() {
-customElements.define('table-v2', TableCustomElement)
-customElements.define('table-configure', TableConfigureCustomElement)
-// }
+export { default as Table } from '~/components/TableV2.vue'
+export { default as TableConfigure } from '~/components/TableConfigure.vue'
+export { default as Pagination } from '~/components/Pagination.vue'
