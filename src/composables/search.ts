1cf72d0978bdef1eb7ab65c4cd750e44186ac706
Chris <chrisbreuer93@gmail.com>
2022-05-03 22:56:46 -0700

    chore: wip

--- src/composables/search.ts
+++ src/composables/search.ts
@@ -1,18 +1,12 @@
-import { isString } from '@vueuse/core'
 import type { Index, SearchResponse } from 'meilisearch'
 import { MeiliSearch } from 'meilisearch'
 import { tableStore } from './table'
 
 export async function search(index: Index, search: string, attrs: object) {
   const q = search.trim()
-  const perPage = tableStore.value.perPage
-
-  if (isString(perPage))
-    tableStore.value.perPage = parseInt(perPage, 10) ?? 20
-
-  const options = { limit: tableStore.value.perPage }
+  const perPage = tableStore.value.perPage as number || 20
+  const options = { limit: perPage }
   const config = attrs
-
   const results: SearchResponse<Record<string, any>> = await index.search(q, options, config)
 
   return results
