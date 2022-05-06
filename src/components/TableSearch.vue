e2c4edb70d55a462ca67f2fc10926f889ba98cb6
Chris <chrisbreuer93@gmail.com>
2022-05-05 22:24:53 -0700

    chore: wip

--- src/components/TableSearch.vue
+++ src/components/TableSearch.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
-// import { tableStore } from '~/composables/table'
+import { tableStore } from '~/composables/table'
 </script>
 
 <template>
-  <div>
-    <label for="search" class="font-medium text-sm text-gray-700 block">Quick search</label>
-    <div class="flex mt-1 relative items-center">
-      <input id="search" type="text" name="search" class="rounded-md border-gray-300 shadow-sm w-full pr-12 block sm:text-sm focus:border-indigo-500 focus:ring-indigo-500">
-      <div class="flex py-1.5 pr-1.5 inset-y-0 right-0 absolute">
-        <kbd class="border rounded font-sans font-medium border-gray-200 text-sm px-2 text-gray-400 inline-flex items-center"> ⌘K </kbd>
-      </div>
-    </div>
-  </div>
+  <input v-model="tableStore.query" type="text" class="">
 </template>
