36ee39df3045af2897f685c4afbce16275e1a2ab
glennmichael123 <gtorregosa@gmail.com>
2026-04-10 17:54:39 +0800

    chore: include md in pickier lint extensions

--- .config/pickier.ts
+++ .config/pickier.ts
@@ -18,7 +18,7 @@ const config: PickierConfig = {
   ],
 
   lint: {
-    extensions: ['ts', 'js'],
+    extensions: ['ts', 'js', 'md'],
     reporter: 'stylish',
     cache: false,
     maxWarnings: -1,
